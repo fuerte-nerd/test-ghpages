d5ba4cbaf61f61687d5ab8577ef35896876a494f
Fuerte Nerd <fuertenerd@gmail.com>
2020-04-18 17:58:00 +0100

    Updates

---
+++ precache-manifest.625574e5998b28d9884ff202c5d51e31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04bc7a476af2a7d4b1b9ae482ffaf138",
+    "url": "/test-ghpages/index.html"
+  },
+  {
+    "revision": "9787bf0612f8a947adb6",
+    "url": "/test-ghpages/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9d7f4aa6b4f7e87a6dcf",
+    "url": "/test-ghpages/static/js/2.5bd8c391.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-ghpages/static/js/2.5bd8c391.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9787bf0612f8a947adb6",
+    "url": "/test-ghpages/static/js/main.341ebe12.chunk.js"
+  },
+  {
+    "revision": "91fa4d3752cbab636a48",
+    "url": "/test-ghpages/static/js/runtime-main.348b59f9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-ghpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
